6327a18139789c1593b5cfa4269922aab1a81d0c
Chandrakant121 <csjoshi989@gmail.com>
2022-08-02 14:21:54 +0530

    backend updated with jwt

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+app.use(express.json());
+
+const { register, login } = require("./controllers/auth.controller");
+
+app.post("/register", register);
+app.post("/login", login);
+
+module.exports = app;
